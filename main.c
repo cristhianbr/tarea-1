00b3754fe09abc57c4eed2c3e30ba47cd9070b7b
cristhianbr <cristhianbr1995@gmail.com>
2015-08-13 12:48:39 -0500

    se modifico el archivo main.c creando un menu para el usuario que permitiera ingresar los datos, escoger la operacion deseado y obtener el resultado de dicha operacion

--- main.c
+++ main.c
@@ -3,5 +3,44 @@
 
 int main (void)
 {
-  return 0;
+	int a,b,i,z;	
+	printf ("Que operacion desea \n1 para Resta\n2 para Suma\n3 para Division\n4 para Multiplicacion");
+	scanf ("%d",&i);
+	printf ("Ingrese el primer valor a los cuales le desea hacer la operacion");
+	scanf ("%d",&a);
+	printf ("\nIngrese el segundo valor");
+        scanf ("%d",&b);
+
+swich (i)
+{
+	case 1:
+               z=resta(a,b);
+               break
+
+        case 2:
+               z=suma(a,b);
+               break
+
+        case 3:
+               z=division(a,b);
+               break
+
+        case 4:
+               z=multiplicacion(a,b);
+               break
+}
+
+printf("El resultado de la operacion es: %d", z);
+
+
+
+
+
+
+
+
+
+} 
+ 
+return 0;
 }
