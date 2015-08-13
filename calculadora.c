addd8d705e8b9146ff382602faf75287f2eb9c22
cristhianbr <cristhianbr1995@gmail.com>
2015-08-13 12:16:53 -0500

    se modifico calculadora.c agregando el desarrollo de las funciones

---
+++ calculadora.c
@@ -0,0 +1,18 @@
+#include "calculadora.h"
+
+int resta(int a,int b)
+{
+	return a-b;
+}
+int suma(int a,int b)
+{
+	return a+b;
+}
+int division(int a,int b)
+{
+	return a/b;
+}
+int multiplicacion(int a,int b)
+{
+	return a*b;
+}
